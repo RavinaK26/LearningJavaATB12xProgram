8356fe4088fd726386cba9512f6c86d7780557db
Ravina <ravina_kudale@bmc.com>
2025-06-21 11:38:15 +0530

    Declare and initialize variables of different primitive data types

---
+++ src/LiveQuestion/DeclareVariable.java
@@ -0,0 +1,20 @@
+package LiveQuestion;
+
+public class DeclareVariable {
+    public static void main(String[] args) {
+
+        //Integer: 25 Double: 3.14 Boolean: true Character: A
+
+        //Declaring the data types
+        int i = 25;
+        double d = 3.14;
+        boolean b = true;
+        char c = 'A';
+
+        //printing the values
+        System.out.println("Integer:" + i);
+        System.out.println("Double:" + d);
+        System.out.println("Boolean:" + b);
+        System.out.println("Character:" + c);
+    }
+}
